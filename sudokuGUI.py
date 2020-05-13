b4a85def0a2a1c571728ad62dfc1d9921772fd78
Chandrakant <vyavaharecg17.it@coep.ac.in>
2020-05-13 21:53:53 +0530

    add getPuzzle and getSolution

--- sudokuGUI.py
+++ sudokuGUI.py
@@ -1,6 +1,7 @@
 import tkinter as tk
+import numpy as np
 
-def create_grid(win, margin, side, width, height):
+def createGrid(win, margin, side, width, height):
     for i in range(10):
         color = "blue" if i % 3 == 0 else "gray"
         x0 = margin
@@ -16,6 +17,20 @@ def create_grid(win, margin, side, width, height):
         sudokuFrame.create_line(x0, y0, x1, y1, fill=color)
 
 
+def getPuzzle(path, puzzle):
+    with open("sudoku.txt", "r") as sudoku_matrix:
+        line = sudoku_matrix.readline()
+        i = 0
+        while line:
+            puzzle[i] = line.split(" ")
+            i += 1
+            line = sudoku_matrix.readline()
+
+
+def getSolution(path, solution):
+    pass
+
+
 if __name__ == '__main__':
     win = tk.Tk()
     win.title("Sudoku")
@@ -23,6 +38,12 @@ if __name__ == '__main__':
     win.resizable(width=False, height=False)
     sudokuFrame = tk.Canvas(win, width=560, height=560)
     sudokuFrame.pack()
-    create_grid(sudokuFrame, 10, 60, 560, 560)
+    createGrid(sudokuFrame, 10, 60, 560, 560)
+
+    path = "sudoku.txt"
+    puzzle = np.zeros((9, 9), dtype=np.uint8)
+    getPuzzle(path, puzzle)
+    solution = np.zeros((9, 9), dtype=np.uint8)
+    getSolution(path, solution)
     
     win.mainloop()
