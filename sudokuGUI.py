8c9533646d21dbd10d229c1e88cda7cfeab41638
Chandrakant <vyavaharecg17.it@coep.ac.in>
2020-05-13 22:21:13 +0530

    add createPuzzle function

--- sudokuGUI.py
+++ sudokuGUI.py
@@ -1,7 +1,11 @@
 import tkinter as tk
 import numpy as np
 
-def createGrid(win, margin, side, width, height):
+def createGrid(sudokuFrame, **kwargs):
+    margin = kwargs['margin']
+    side = kwargs['length']
+    width = kwargs['w']
+    height = kwargs['h']
     for i in range(10):
         color = "blue" if i % 3 == 0 else "gray"
         x0 = margin
@@ -31,6 +35,16 @@ def getSolution(path, solution):
     pass
 
 
+def createPuzzle(sudokuFrame, puzzle, **kwargs):
+    for i in range(9):
+        for j in range(9):
+            x = kwargs['margin'] + i * kwargs['length'] + kwargs['length'] // 2
+            y = kwargs['margin'] + j * kwargs['length'] + kwargs['length'] // 2
+            num = puzzle[i][j]
+            if num != 0:
+                sudokuFrame.create_text(x, y, text=num, tags='numbers', font=("Pursia", 20))
+
+
 if __name__ == '__main__':
     win = tk.Tk()
     win.title("Sudoku")
@@ -38,12 +52,14 @@ if __name__ == '__main__':
     win.resizable(width=False, height=False)
     sudokuFrame = tk.Canvas(win, width=560, height=560)
     sudokuFrame.pack()
-    createGrid(sudokuFrame, 10, 60, 560, 560)
+    createGrid(sudokuFrame, margin=10, length=60, w=560, h=560)
 
     path = "sudoku.txt"
     puzzle = np.zeros((9, 9), dtype=np.uint8)
     getPuzzle(path, puzzle)
     solution = np.zeros((9, 9), dtype=np.uint8)
     getSolution(path, solution)
+
+    createPuzzle(sudokuFrame, puzzle, margin=10, length=60)
     
     win.mainloop()
